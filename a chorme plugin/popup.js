41eb1a9a7824bf8e2b3cd12b6f79430dce4881b5
Runner <841766635@qq.com>
2016-11-17 14:50:59 +0800

    点击插件图标运行。发送一个消息给content script

--- a chorme plugin/popup.js
+++ a chorme plugin/popup.js
@@ -1,17 +1,12 @@
-//在新窗口打开
-var a = {
-	aBlank:function(){		
-		var aDoms = document.getElementsByTagName('a');
-		var len = aDoms.length;
-		for (var i = 0; i < len; i++) {
-			aDoms[i].setAttribute("target","_blank")
-		}
-	}
-}	
 
-a.aBlank();
-//有些网页是用ajax加载很多内容出来，里面包装a标签。所以设置一个定时器。定期执行。
-var timer = setInterval(function(){
-	a.aBlank();
-}, 1500);
+// var btn = document.getElementById('run');
 
+// btn.onclick = function(){
+
+	chrome.tabs.getSelected(null, function(tab) {
+		chrome.tabs.sendRequest(tab.id, {greeting: "hello"}, function(response) {
+			// console.log(response.farewell);
+		});
+	});
+
+// }
