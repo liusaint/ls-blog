edef1db80fe4b4d77d1e0798a196391f949f274c
Runner <841766635@qq.com>
2018-06-16 11:59:00 +0800

    一些优化

--- code/mi/back.js
+++ code/mi/back.js
@@ -17,7 +17,7 @@ var app = {
 		} catch (e) {
 			// 还没加载出来
 		}
-		this.timer = setTimeout(this.setTimer.bind(this), 50);
+		this.timer = setTimeout(this.setTimer.bind(this), 30);
 	},
 	//商品页面做的事情
 	detailFn() {
@@ -29,12 +29,12 @@ var app = {
 		$("#J_miAlertConfirm")[0] && $("#J_miAlertConfirm")[0].click();
 
 
-		//版本
+		//版本 128g
 		var levelDom = $(".J_step.pro-choose[data-index=0] ul>li").eq(1)[0];
-		//颜色
-		var colorDom = $(".J_step.pro-choose[data-index=1] ul>li").eq(0)[0];
-		//保险
-		var safeDom = $(".J_service.pro-choose[data-index=0] ul>li").eq(0)[0];
+		//颜色 白色
+		var colorDom = $(".J_step.pro-choose[data-index=1] ul>li").eq(1)[0];
+		//保险 
+		var safeDom = $(".J_service.pro-choose[data-index=0] ul>li").eq(1)[0];
 		//当这几个都加载出来的时候才执行点击加入。避免购物车页面出现多项商品。
 		if (levelDom && colorDom && safeDom) {
 			levelDom.click();
@@ -50,7 +50,14 @@ var app = {
 	},
 	//跳转到购物车页面
 	goCart() {
-		$(".J_actBox a:contains('去购物车结算')")[0] && $(".J_actBox a:contains('去购物车结算')")[0].click();
+
+		if ($(".J_actBox a:contains('去购物车结算')")[0]) {
+			$(".J_actBox a:contains('去购物车结算')")[0].click();
+			//观察到卡了一s在这里，所以加上这一句
+			location.href = 'https://static.mi.com/cart/';
+			//跑一个错误，避免再次执行上面那一句。 
+			throw new Error('abc');
+		}
 	},
 	//购物车页面的事情 
 	cartFn() {
@@ -63,15 +70,32 @@ var app = {
 		});
 		//确认删除按钮
 		$("#J_alertOk")[0] && $("#J_alertOk")[0].click();
+		var interval = 0;
 		//将每一条的数量减少到1。
 		$(".J_minus").each(function(index, el) {
 			//数量不为1就减少。
-			while($(el).next('input').val()!=1){
+			//不能用这个while。因为input中的值是异步减少的。
+			if ($(el).next('input').val() != 1) {
+				interval = 200;
+				el.click();
+				el.click();
+				el.click();
 				el.click();
 			}
 		});
 		//去结算
-		$("#J_goCheckout")[0] && $("#J_goCheckout")[0].click();
+		setTimeout(function() {
+			$("#J_goCheckout")[0] && $("#J_goCheckout")[0].click();
+		}, interval);
+		//如果检测到这句话就刷新购物车页面。
+		//因为系统问题或者
+		if ($('h3:contains(抱歉，以下商品已经失效或者暂时售罄):visible').length > 0) {
+			setTimeout(function() {
+				location.reload();
+			}, 500)
+
+		}
+
 	},
 	//选地址，下单页面
 	orderFn() {
