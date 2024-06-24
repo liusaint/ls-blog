4137bf5976129e8bb9bfe6d50b1617be53e50cc7
刘圣 <sheng.liu@ximalaya.com>
2024-06-24 18:13:13 +0800

    优化，避免消耗过快

--- ChatgptAutoContinue/content.js
+++ ChatgptAutoContinue/content.js
@@ -1,29 +1,37 @@
+let timer = null;
+lastClickTime = 0;
 function clickButton() {
-  chrome.storage.sync.get('enabled', (data) => {
+  chrome.storage.sync.get("enabled", (data) => {
     if (data.enabled) {
-      const buttons = document.querySelectorAll('.btn.relative.btn-secondary');
+      const buttons = document.querySelectorAll(".btn.relative.btn-secondary");
       let targetButton = null;
 
-      buttons.forEach(button => {
-        if (button.textContent.includes('继续生成')) {
+      buttons.forEach((button) => {
+        if (button.textContent.includes("继续生成")) {
           targetButton = button;
         }
       });
 
       if (targetButton) {
+        if (Date.now() - lastClickTime < 5000) {
+          console.log("Button clicked too soon");
+          return;
+        }
         targetButton.click();
-        console.log('Button clicked');
-        // 8 seconds later, check and click again
-        setTimeout(clickButton, 8000);
+        lastClickTime = Date.now();
+        console.log("Button clicked");
+        // 30 seconds later, check and click again
+        clearTimeout(timer);
+        timer = setTimeout(clickButton, 40000);
       } else {
-        console.log('Button not found');
+        console.log("Button not found");
+        clearTimeout(timer);
         // Retry after 2 seconds if button not found
-        setTimeout(clickButton, 2000);
+        timer = setTimeout(clickButton, 2000);
       }
     }
   });
 }
-
 
 // Listen for messages from the background script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
