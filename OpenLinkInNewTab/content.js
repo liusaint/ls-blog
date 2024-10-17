e85a0e093a992dce8f7af76ff2ea09a0dffa698e
Runner <841766635@qq.com>
2024-10-18 00:08:30 +0800

    Update content.js

--- OpenLinkInNewTab/content.js
+++ OpenLinkInNewTab/content.js
@@ -27,15 +27,28 @@ getStorageDomain();
 document.body.addEventListener(
   "click",
   function (event) {
+    let targetElement = event.target;
+    let linkElement = null;
+
+    // 检查点击元素本身或其父元素是否为 A 标签
+    while (targetElement && targetElement !== document.body) {
+      if (targetElement.tagName === "A") {
+        linkElement = targetElement;
+        break;
+      }
+      targetElement = targetElement.parentElement;
+    }
+
     if (
-      event.target.tagName === "A" &&
-      event.target.href &&
-      !event.target.target
+      linkElement &&
+      linkElement.href &&
+      !linkElement.target
     ) {
       const hostname = new URL(window.location.href).hostname;
       if (domainList.includes(hostname)) {
         event.preventDefault(); // 确保阻止默认行为
-        window.open(event.target.href, "_blank"); // 在新窗口中打开链接
+        event.stopPropagation();
+        window.open(linkElement.href, "_blank"); // 在新窗口中打开链接
       }
     }
   },
